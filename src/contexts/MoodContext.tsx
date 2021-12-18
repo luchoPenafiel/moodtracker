b4b8a06f44163994ef9dcf657b1d254f01c0911d
Luciano Peñafiel <luciano.penafiel@aerolab.co>
2021-12-18 12:09:29 -0300

    add async storage

--- src/contexts/MoodContext.tsx
+++ src/contexts/MoodContext.tsx
@@ -1,8 +1,32 @@
 // Vendor
-import React, { useContext, createContext, ReactElement, ReactNode, useState, useCallback } from 'react';
+import React, { useContext, createContext, ReactElement, ReactNode, useState, useCallback, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Types
 import { MoodOptionType, MoodOptionWithTimestamp } from '@moodtracker/types/types';
+
+type AppData = {
+  moodList: MoodOptionWithTimestamp[];
+};
+
+const KEY = 'moodtracker-data';
+
+const setAppData = async (data: AppData) => {
+  try {
+    await AsyncStorage.setItem(KEY, JSON.stringify(data));
+  } catch {}
+};
+
+const getAppData = async (): Promise<AppData | null> => {
+  try {
+    const response = await AsyncStorage.getItem(KEY);
+    if (response) {
+      return JSON.parse(response);
+    }
+  } catch {}
+
+  return null;
+};
 
 type MoodContextType = {
   moodList: MoodOptionWithTimestamp[];
@@ -22,7 +46,23 @@ export const MoodContextProvider = ({ children }: Props): ReactElement => {
   const [moodList, setMoodList] = useState<MoodOptionWithTimestamp[]>([]);
 
   const handleSelectMood = useCallback((selectedMood: MoodOptionType) => {
-    setMoodList(current => [...current, { mood: selectedMood, timestamp: Date.now() }]);
+    setMoodList(current => {
+      const newMoodList = [...current, { mood: selectedMood, timestamp: Date.now() }];
+
+      setAppData({ moodList: newMoodList });
+
+      return newMoodList;
+    });
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      const data = await getAppData();
+
+      if (data) {
+        setMoodList(data.moodList);
+      }
+    })();
   }, []);
 
   return <MoodContext.Provider value={{ moodList, handleSelectMood }}>{children}</MoodContext.Provider>;
