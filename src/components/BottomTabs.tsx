0c3b46e6ceaddb9425804d4f5f0fa3d4b0ab10f6
Luciano Peñafiel <luciano.penafiel@aerolab.co>
2021-12-20 18:58:20 -0300

    add icon to bottom bar navigation

--- src/components/BottomTabs.tsx
+++ src/components/BottomTabs.tsx
@@ -4,12 +4,22 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 // Screens
 import { Home, History, Analytics } from '@moodtracker/screens';
+import { Text } from 'react-native';
+import { HomeIcon } from './icons/Home';
 
 const BottomTabs = createBottomTabNavigator();
 
 export const BottomTabsNavigator = (): ReactElement => {
   return (
-    <BottomTabs.Navigator>
+    <BottomTabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: () => {
+          if (route.name === 'Home') {
+            return <HomeIcon size={20} />;
+          }
+          return <Text>{route.name}</Text>;
+        },
+      })}>
       <BottomTabs.Screen name="Home" component={Home} />
       <BottomTabs.Screen name="History" component={History} />
       <BottomTabs.Screen name="Analytics" component={Analytics} />
