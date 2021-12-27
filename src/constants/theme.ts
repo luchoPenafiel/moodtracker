089f8566355f14a08299344c27b19a7ece28e95a
Luciano Peñafiel <luciano.penafiel@aerolab.co>
2021-12-27 19:14:42 -0300

    add analytics screen

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -2,6 +2,9 @@ export const theme = {
   color: {
     purple: '#454c73',
     white: '#fff',
+    lavender: '#87677B',
+    blue: '#1D84B5',
+    grey: '#8E9AAF',
   },
   font: {
     bold: 'Kalam-Bold',
--- src/screens/Analytics.screen.tsx
+++ src/screens/Analytics.screen.tsx
@@ -1,10 +1,39 @@
+// Vendor
 import React, { ReactElement } from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { VictoryPie } from 'victory-native';
+import groupBy from 'lodash/groupBy';
+
+// Context
+import { useMoodContext } from '@moodtracker/contexts/MoodContext';
+import { theme } from '@moodtracker/constants/theme';
 
 export const Analytics = (): ReactElement => {
+  const moodContext = useMoodContext();
+
+  const data = Object.entries(groupBy(moodContext.moodList, 'mood.emoji')).map(([key, value]) => ({
+    x: key,
+    y: value.length,
+  }));
+
   return (
-    <View>
-      <Text>Analytics</Text>
+    <View style={styles.container}>
+      <VictoryPie
+        labelRadius={80}
+        radius={150}
+        innerRadius={50}
+        colorScale={[theme.color.purple, theme.color.lavender, theme.color.blue, theme.color.grey, theme.color.white]}
+        style={{ labels: { fontSize: 30 } }}
+        data={data}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
